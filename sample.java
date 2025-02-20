0ecdd006a4003dfc5630546a985291988eee8681
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-02-19 19:21:17 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -33,3 +33,38 @@ class Solution {
         return calc;
     }
 }
+
+
+class Solution {
+    String[] below_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Nighty"};
+    public String numberToWords(int num) {
+        String result= "";
+        if (num == 0) {
+            return "zero";
+        }
+        String[] thousands = {"", "Thousand", "Million", "Billion"};
+        int i=0;
+        while (num > 0) {
+            if (num %1000 != 0) {
+                result = recurse(num % 1000) + thousands[i]+ " " + result;
+            }
+            num = num/1000;
+            i++;
+        }
+        return result.trim();
+    }
+    private String recurse(int num) {
+        if (num == 0) {
+            return "";
+        }
+        else if (num < 20) {
+            return below_20[num] + " ";
+        }
+        else if (num <100) {
+            return tens[num/10] + " " + recurse(num%10);
+        } else {
+            return below_20[num/100] + " Hundred " + recurse(num%100);
+        }
+    }
+}
